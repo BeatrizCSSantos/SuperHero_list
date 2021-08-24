4f6b0ab1ea15262e11e3981133468e7d14dacbbb
Beatriz C.S. Santos <beatrizcs96@hotmail.com>
2021-08-24 10:05:06 -0300

    Add Modal and style hero

--- react-superhero/src/SearchResultItem.js
+++ react-superhero/src/SearchResultItem.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+
+Modal.setAppElement('#root')
 
 function SearchResultItem(props) {
+  const [ modalIsOpen, setModalIsOpen] = useState(false);
   const {data} = props;
 
   console.log('data', data);
@@ -11,11 +16,25 @@ function SearchResultItem(props) {
       </div>
       <div className="right">
         <h1>{data.name}</h1>
-        <span style={{ color:'gray', marginBottom: 5 }}>{data.biography['full-name']}</span>
+        <span style={{ color:'rgba(8, 7, 7, 0.842)', marginBottom: 5 }}>{data.biography['full-name']}</span>
         <div className="stats">
-          <div>strength: {data.powerstats.strength}</div>
-          <div>speed: {data.powerstats.speed}</div>
-          <div>power: {data.powerstats.power}</div>
+          <button className="open-button" onClick={() => setModalIsOpen(true)}> Ver informações </button>
+          <div>
+          <Modal className="modal-info" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+          <div className="details">
+          <div lassName="strength-space"> . </div>
+          <div className="strength">strength: {data.powerstats.strength}</div>
+          <div className="speed">speed: {data.powerstats.speed}</div>
+          <div className="power">power: {data.powerstats.power}</div>
+          </div>
+          <div>
+            <button className="close-button" onClick={() => setModalIsOpen(false)}>
+              Fechar informações
+            </button>
+          </div>
+          </Modal>
+          </div>
+
         </div>
       </div>
     </div>
